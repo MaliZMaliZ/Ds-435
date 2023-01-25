046eb4f02c34852d4cda4bcf6529caaa15e28ad3
MaliZMaliZ <123527770+MaliZMaliZ@users.noreply.github.com>
2023-01-25 10:27:12 +0700

    Update revers.ts

--- revers.ts
+++ revers.ts
@@ -1,15 +1,3 @@
-//const x = "hello"
-//console.log(x.substring(1)) //ello
-//console.log(x.charAt(0)) //h
-
-function reverseString(text: string) {
-    let reverseText = ""
-    for (let i = text.length - 1; i >= 0; i--) {
-        reverseText += text[i]
-    }
-    return reverseText
-}
-//console.log(reverseString(x))//olleh
 
 function RrSt(text: string):string {
     if (text === '')     
